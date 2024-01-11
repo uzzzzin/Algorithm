02719056498a66b154f1aa9a8e10d71dd4441e86
uzzzzin <105213685+uzzzzin@users.noreply.github.com>
2024-01-12 06:12:56 +0900

    [level Lv.2] Title: [1차] 캐시, Time: 0.42 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) 
+{
+    int accTime = 0;
+    
+    if(0 == cacheSize)
+        return 5* cities.size();
+    
+    vector<string> cache;
+    
+    for(int i = 0; i < cities.size(); ++i)
+    {
+        for(int j = 0 ; j < cities[i].length(); ++j)
+            cities[i][j] = tolower(cities[i][j]);
+    }
+    
+    // [ "seoul", "seoul", "seoul", "tokyo", "seoul", "tokyo" ]
+    
+    cache.push_back(cities[0]);
+    accTime += 5;
+    
+    for(int i = 1; i < cities.size(); ++i)
+    {
+        auto iter = find(cache.begin(), cache.end(), cities[i]);
+        
+        if(iter != cache.end()) // cache hit !!
+        {
+            cache.erase(iter);
+            cache.push_back(cities[i]);
+            accTime += 1;
+        }
+        else // cache miss ㅠㅠ
+        {
+            if(cacheSize <= cache.size())
+                cache.erase(cache.begin());    
+            cache.push_back(cities[i]);
+            accTime += 5;
+        }
+    }
+    return accTime;
+}
